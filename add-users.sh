31f7fa6cea05ad83ecfdc5a5fada07021607b3e9
Chris Jones <christopheralan88@gmail.com>
2018-09-17 22:39:28 -0400

    Added validation to add-users.sh to check that script is run as root user.

--- add-users.sh
+++ add-users.sh
@@ -3,6 +3,13 @@
 #This script creates an account on the local system.
 #You will be prompted for the account name and password.
 
+#First check that this script is being executed as the root user
+if [ "$(id -u)" != 0 ] ; 
+then
+	echo 'You need to be root to run this script because it adds users.'
+	exit 1
+fi
+
 #Ask for the user name.
 read -p 'Type the username to create:  ' USER_NAME
 
@@ -15,13 +22,7 @@ read -p 'Type the password of the account to create:  ' PASSWORD
 #Create the user.
 useradd -c "${COMMENT}" -m ${USER_NAME}
 
-  #Check if user is root
-  #if [[ $(id -u) != 0 ]] then
-	#echo 'You need to be root to run this script because it adds users.'
-	#exit(1)
-  #fi
 #Set the password for the user.
-#echo ${PASSWORD} | chpasswd "${USER_NAME}:${PASSWORD}"
 echo "${USER_NAME}:${PASSWORD}" | chpasswd
 
 #Force password change on first login.
